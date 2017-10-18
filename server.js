a0e34c64aecd59c500a35eaae6d62182910077cc
Daniel Huigens <d.huigens@gmail.com>
2017-10-18 21:59:05 +0200

    Check /current against GitHub in Service Worker

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ require('newrelic');
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+	if(req.method === 'GET') {
+		res.set('X-GitHub-Commit', process.env.HEROKU_SLUG_COMMIT);
+	}
+	next();
+});
+
 app.get(/^\/(v2\/)?current$/, function(req, res) {
 	res.set('Content-Type', 'application/zip');
 	res.sendfile(req.path.substr(1).replace('/', '-'));
